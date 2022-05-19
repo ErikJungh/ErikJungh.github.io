979a225cda7f97975549065ed63dc3da102b23a4
Erik Junghahn <erik.junghahn@gmail.com>
2022-05-20 01:01:07 +0200

    Fixed alignemnt issue

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -4,10 +4,14 @@ import { Link } from "react-router-dom";
 function CardItem(props) {
   return (
     <>
-      <li className="cards__item">
-        <div className="cards__item__link">
+      <li className="about__cards__item">
+        <div className="about__cards__item__link">
           <figure className="cards__picture__pic-wrap">
-            <img src={props.src} alt="app" className="cards__item__img" />
+            <img
+              src={props.src}
+              alt="app"
+              className="about__cards__item__img"
+            />
           </figure>
         </div>
       </li>
