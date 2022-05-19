28cf05383930f6807b4b93396ec0322c6e57497a
Erik Junghahn <erik.junghahn@gmail.com>
2022-05-19 21:10:23 +0200

    new build

--- src/components/AppCardItem.js
+++ src/components/AppCardItem.js
@@ -33,6 +33,7 @@ function AppCardItem(props) {
                   autoPlay
                   loop
                   muted
+                  playsinline
                 />
               </InView>
             ) : (
